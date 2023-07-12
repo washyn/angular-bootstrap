8af8df93c67e4eb846a52f97b07476e4b4531454
Washington Acero M <shin321654987@gmail.com>
2023-07-11 19:41:51 -0700

    generado proxy services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookDto, BookService } from './proxy';
+import { PagedAndSortedResultRequestDto, PagedResultDto } from '@abp/ng.core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-bootstrap';
+
+  books = {
+    items: [],
+    totalCount: 0,
+  } as PagedResultDto<BookDto>;
+
+  constructor(private bookService: BookService) {}
+  ngOnInit(): void {
+    this.bookService
+      .getList({} as PagedAndSortedResultRequestDto)
+      .subscribe((response) => {
+        this.books = response;
+      });
+  }
 }
